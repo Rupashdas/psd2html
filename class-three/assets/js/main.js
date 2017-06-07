2cda2529a43601ccc6d46f19ccc3f30c6239b78e
Rupashdas <rupash.das.40@gmail.com>
2017-06-07 20:34:50 +0600

    class-three

---
+++ class-three/assets/js/main.js
@@ -0,0 +1,39 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+		$(".case-study-carousel").owlCarousel({
+			items: 3,
+			margin: 30,
+			loop: true,
+			dots: true,
+			nav: false,
+		});
+		$(".testimonial-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			nav: false,
+		});
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+	new WOW().init();
+
+}(jQuery));	
